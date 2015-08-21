c7abca70acf59afef616b427d77e58c58c9b8fc6
fuyb <been1986@gmail.com>
2015-08-21 10:30:30 +0800

    添加用户时不输入密码, 使用 /dev/urandom 生成一个伪随机密码。

--- manage-vpn-user.sh
+++ manage-vpn-user.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 usage() {
-    echo "usage: $0 <add> <username> <passwd>
-                 $0 <del> <username>"
+    echo "usage: $0 <add> <username>
+       $0 <del> <username>"
     exit 1
 }
 
 [ $# -lt 2 ] && usage
 [ "$1" != "add" -a "$1" != "del" ] && usage
-[ "$1" = "add" -a "$#" -ne 3 ] && usage
-[ "$1" = "del" -a "$#" -ne 2 ] && usage
 
 USER=$2
 CHAP_FILE="/etc/ppp/chap-secrets"
@@ -17,7 +15,7 @@ CHAP_FILE="/etc/ppp/chap-secrets"
 cp $CHAP_FILE $CHAP_FILE.old
 
 if [ "$1" = "add" ]; then
-    PASSWD=$3
+    PASSWD="$(< /dev/urandom tr -dC [:alnum:] | head -c 9)"
     echo "$USER pptpd $PASSWD *" >> $CHAP_FILE
     tail -1 $CHAP_FILE
 elif [ "$1" = "del" ]; then
