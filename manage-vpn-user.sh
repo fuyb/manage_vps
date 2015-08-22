e3aafb3dada49c3a98b3b30b415638be3cea8cc4
fuyb <been1986@gmail.com>
2015-08-22 23:16:24 +0800

    1. 使用 case 语句处理第一个命令参数。 2. 函数化添加删除代码。 3. 删除用户时 sed 表达式的 bug, 不需要用 /^$user*$/d， 只需要 /$user/d 就可以。

--- manage-vpn-user.sh
+++ manage-vpn-user.sh
@@ -9,17 +9,31 @@ usage() {
 [ $# -lt 2 ] && usage
 [ "$1" != "add" -a "$1" != "del" ] && usage
 
-USER=$2
 CHAP_FILE="/etc/ppp/chap-secrets"
 
 cp $CHAP_FILE $CHAP_FILE.old
-
-if [ "$1" = "add" ]; then
-    PASSWD="$(< /dev/urandom tr -dC [:alnum:] | head -c 9)"
-    echo "$USER pptpd $PASSWD *" >> $CHAP_FILE
+add_user() {
+    local user=$1
+    local passwd="$(< /dev/urandom tr -dC [:alnum:] | head -c 9)"
+    echo "$user pptpd $passwd *" >> $CHAP_FILE
     tail -1 $CHAP_FILE
-elif [ "$1" = "del" ]; then
-    sed -i "/^$USER.*$/d" $CHAP_FILE
-fi
+}
+
+del_user() {
+    local user=$1
+    sed -i "/$user/d" $CHAP_FILE
+}
+
+case $1 in
+    "add")
+        add_user $2
+        ;;
+    "del")
+        del_user $2
+        ;;
+    *)
+        usage
+        ;;
+esac
 
 exit 0
