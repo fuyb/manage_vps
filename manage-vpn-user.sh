29cdee785b8ceee7a77b3e7bf4d9103467ed74bf
fuyb <been1986@gmail.com>
2015-08-22 23:50:38 +0800

    1. 增加 update user 操作。 如果 user 不存在，询问是否 add user. 2. 使用 -a -d -u 代替 add del update 参数。

--- manage-vpn-user.sh
+++ manage-vpn-user.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
 usage() {
-    echo "usage: $0 <add> <username>
-       $0 <del> <username>"
+    echo "usage: $0 -a -d -u <username>"
     exit 1
 }
 
 [ $# -lt 2 ] && usage
-[ "$1" != "add" -a "$1" != "del" ] && usage
 
 CHAP_FILE="/etc/ppp/chap-secrets"
-
 cp $CHAP_FILE $CHAP_FILE.old
+
 add_user() {
     local user=$1
     local passwd="$(< /dev/urandom tr -dC [:alnum:] | head -c 9)"
@@ -23,7 +21,7 @@ del_user() {
     local user=$1
     local n=$(grep -c "$user" $CHAP_FILE)
     if [ $n -gt 0 ]; then
-        echo -n "Delete user [Y/N]: "
+        echo -n "Delete user [Y/N]?: "
         read option
         if [ "$option" == "y" -o "$option" == "Y" ]; then
             sed -i "/$user/d" $CHAP_FILE
@@ -33,12 +31,34 @@ del_user() {
     fi
 }
 
+update_user() {
+    local user=$1
+    local n=$(grep -c "$user" $CHAP_FILE)
+    if [ $n -gt 0 ]; then
+        echo -n "Update user [Y/N]?: "
+        read option
+        if [ "$option" == "y" -o "$option" == "Y" ]; then
+            local passwd="$(< /dev/urandom tr -dC [:alnum:] | head -c 9)"
+            local info="$user pptpd $passwd *"
+            sed -i "s/^$user.*$/$info/g" $CHAP_FILE
+            grep "$user" $CHAP_FILE
+        fi
+    else
+        echo -n "User does not exists, would you want add user [Y/N]?: "
+        read option
+        [ "$option" == "y" -o "$option" == "Y" ] && add_user $user
+    fi
+}
+
 case $1 in
-    "add")
+    "-a")
         add_user $2
         ;;
-    "del")
+    "-d")
         del_user $2
+        ;;
+    "-u")
+        update_user $2
         ;;
     *)
         usage
