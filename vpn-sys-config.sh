4ccdec78a86fc28737ef3375db9e67f0d294c7c4
fuyb <been1986@gmail.com>
2015-08-30 17:55:56 +0800

    add client talk to each other

--- vpn-sys-config.sh
+++ vpn-sys-config.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
-if [ $# -gt 1 ]; then
+if [ $# -lt 1 ]; then
     echo "usage: $0 [wan interface]"
     exit 1
 fi
 
-# iptables config
-IPTABLES="/sbin/iptables -t nat -I POSTROUTING"
+IPTABLES="/sbin/iptables"
 if [ $# -ne 1 ]; then
     WAN_INTERFACE="$(route -n | head -3 | tail -1 | awk '{print $8}')"
 else
     WAN_INTERFACE="$1"
 fi
-$IPTABLES -o $WAN_INTERFACE -j MASQUERADE
+
+# iptables config
+$IPTABLES -t nat -I POSTROUTING -o $WAN_INTERFACE -j MASQUERADE
+echo -n "Enable client talk to each other: [Y/N]?"
+read client_talk
+if [ "$client_talk" == "Y" -o "$client_talk" == "y" ]; then
+    # client talk to each other config
+    $IPTABLES -I INPUT -s 10.0.0.0/8 -i ppp0 -j ACCEPT
+    $IPTABLES -A FORWARD -o $WAN_INTERFACE -j ACCEPT
+fi
 /sbin/iptables-save
 
 # system control config
